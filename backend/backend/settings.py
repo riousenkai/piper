abbf48221449c11ae655c846ee1689b4c5ea6452
riousenkai <johnelijah.fajardo@gmail.com>
2022-01-30 00:57:28 -0600

    postgres db communicating with backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
