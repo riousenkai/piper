c8dd76f071b04e2196cfad8b98cf5047f4e91450
riousenkai <johnelijah.fajardo@gmail.com>
2022-01-30 01:28:00 -0600

    basic models added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'piper.apps.PiperConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- backend/piper/models.py
+++ backend/piper/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=100)
+    image_url = models.CharField(max_length=255)
+
+    def to_dict(self):
+        return {
+            'username': self.username,
+            'password': self.password,
+            'image_url': self.image_url
+        }
+
+
+class Locations(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'address': self.address
+        }
+
+class Saved(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    location_id = models.ForeignKey(Locations, on_delete=models.CASCADE)
+
+    def to_dict(self):
+        return {
+            'user_id': self.user_id,
+            'location_id': self.location_id
+        }
