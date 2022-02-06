b854a3890fb3c70811b37d73b0121f6cb8451f7a
riousenkai <johnelijah.fajardo@gmail.com>
2022-02-05 22:42:05 -0600

    added login cred

--- frontend/src/store/components/Login/Login.js
+++ frontend/src/store/components/Login/Login.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
 
 const Login = () => {
+  const [loginCred, setLoginCred] = useState('')
+  const [pass, setPass] = useState('')
 
   return (
     <div>
       <div>Log in to use Piper</div>
+      <input type="text" value={loginCred} onChange={(e) => setLoginCred(e.target.value)} />
+      <input type="password" value={pass} onChange={(e) => setPass(e.target.value)} />
     </div>
   );
 };
