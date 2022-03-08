48273866485a3fe6dc00c371b8d473fcd46ee8a5
riousenkai <johnelijah.fajardo@gmail.com>
2022-03-07 21:22:44 -0600

    added fixes to index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import configureStore from "./store";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from "react-redux";
 
 const store = configureStore();
